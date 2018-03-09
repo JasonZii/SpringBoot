c437128b190deb24f66f19a4604c06d9209fb095
JasonZii <jasonzii@outlook.com>
2018-03-09 22:56:55 +0800

    SpringBoot代码提交

---
+++ src/main/java/com/jasonzii/aspect/HttpAspect.java
@@ -0,0 +1,60 @@
+package com.jasonzii.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @Author : jasonzii @Author
+ * @Description :
+ * @CreateDate : 18.3.9  19:12
+ */
+@Aspect
+@Component
+public class HttpAspect {
+
+    private final static Logger logger = LoggerFactory.getLogger(HttpAspect.class);
+
+
+    //切哪个点
+    @Pointcut("execution(public * com.jasonzii.controller.GirlController.*(..))")
+    public void log(){
+    }
+
+    @Before("log()")
+    public void doBefore(){
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        //url
+        logger.info("url={}",request.getRequestURL());
+
+        //method
+        logger.info("method={}",request.getMethod());
+
+        //ip
+        logger.info("ip={}",request.getRemoteAddr());
+
+        //类方法
+   //     logger.info("class_method={}", );
+
+        //参数
+   //     logger.info("args={}", );
+    }
+
+    @After("log()")
+    public void doAfter(){
+        logger.info("2222222222222");
+    }
+
+    @AfterReturning(returning = "object", pointcut = "log()")
+    public void doAfterReturning(Object object){
+        logger.info("response={}",object.toString());
+    }
+}
